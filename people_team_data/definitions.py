05cfd2b2689606456f362c4558c89d30ea9e4a3c
Jack Stehn <jack@caliberschools.org>
2025-05-19 21:59:30 +0000

    remove unnecessary dbt asset defintions

--- people_team_data/definitions.py
+++ people_team_data/definitions.py
@@ -1,33 +1,6 @@
-from dagster import (
-    AssetExecutionContext,
-    Definitions,
-    EnvVar,
-    define_asset_job,
-    load_assets_from_modules,
-)
-from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
+from dagster import Definitions, define_asset_job, load_assets_from_modules
 
 from . import assets, resources
-
-# 1. Load dbt project
-dbt_project_directory = EnvVar("DBT_PROJECT_DIR").get_value()
-dbt_profiles_directory = EnvVar("DBT_PROFILES_DIR").get_value()
-dbt_project = DbtProject(
-    project_dir=dbt_project_directory, profiles_dir=dbt_profiles_directory
-)
-print(f"Project directory: {dbt_project_directory}")
-
-# 2. Prepare dbt project if dev
-dbt_project.prepare_if_dev()
-
-
-# 3. Define dbt assets
-@dbt_assets(
-    manifest=dbt_project.manifest_path,
-)
-def dbt_models(context: AssetExecutionContext, dbt: DbtCliResource):
-    yield from dbt.cli(["build"], context=context).stream()
-
 
 # 4. Load all other assets
 all_assets = load_assets_from_modules([assets])
@@ -37,7 +10,7 @@ all_assets_job = define_asset_job(name="all_assets_job")
 env_resources = resources.get_environment_resources()
 # 6. Create definitions object
 defs = Definitions(
-    assets=[*all_assets, dbt_models],  # Include dbt assets here
+    assets=[*all_assets],
     jobs=[all_assets_job],
     schedules=[],
     sensors=[],
