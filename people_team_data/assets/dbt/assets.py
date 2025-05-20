0800be58e300e8d6e589ec8dc93b0ce0ec4ecff8
Jack Stehn <jack@caliberschools.org>
2025-05-20 23:15:48 +0000

    Fix missing manifest path

--- people_team_data/assets/dbt/assets.py
+++ people_team_data/assets/dbt/assets.py
@@ -8,7 +8,7 @@ from .project import (
 )
 
 
-@dbt_assets(project=dbt_project)
+@dbt_assets(manifest=dbt_project.manifest_path, project=dbt_project)
 def dbt_models_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     logger = get_dagster_logger()
 
